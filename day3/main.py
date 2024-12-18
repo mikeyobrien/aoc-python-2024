7a03895c7bef2b17cd611cb85a01eebc0fdbd247
Mikey O'Brien <me@mikeyobrien.com>
2024-12-17 21:23:40 -0600

    day3 part2

--- day3/main.py
+++ day3/main.py
@@ -1,7 +1,7 @@
 import re
 from functools import reduce
 
-pattern = r'mul\(\d{1,3},\d{1,3}\)'
+pattern = r'mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)'
 num_pattern = r'\d{1,3}'
 
 with open('input.txt', 'r') as f:
@@ -9,8 +9,18 @@ with open('input.txt', 'r') as f:
     print(data)
     matches = re.findall(pattern, data)
     res = 0
+    doCompute = True
     for match in matches:
-        nums = re.findall(num_pattern, match)
-        res += reduce(lambda x, y: int(x) * int(y), nums)
+        print(match)
+        if match.startswith('don'):
+            doCompute = False
+            print("Disabled")
+        elif match.startswith('do'):
+            doCompute = True
+            print("Enabled")
+        else:
+            if doCompute:
+                nums = re.findall(num_pattern, match)
+                res += reduce(lambda x, y: int(x) * int(y), nums)
     print(res)
 
